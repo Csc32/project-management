158d2488d4399dd7dc91ed786c50f0a343370f32
csc32 <sanzonettycar@gmail.com>
2023-11-25 12:06:58 -0400

    feat(models): add new models and update relations
    add: relationships with new category and user tables

--- app/Models/Projects.php
+++ app/Models/Projects.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Projects extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "resume",
+    ];
+
+    public function user(): belongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -56,5 +57,10 @@ class User extends Authenticatable
     public function pfg(): BelongsTo
     {
         return $this->belongsTo(Pfgs::class);
+    }
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class);
     }
 }

baf88afad4a9996e7256c6b83fa2969af9ce4d3d
csc32 <sanzonettycar@gmail.com>
2023-11-25 16:59:35 -0400

    feat(web): add new styles and colors
    add: Montserrat font
    update: config to add error color
    fix(color): problem with red color in tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
             backgroundImage: {
                 thumbnail: "url(/public/images/gestion_proyecto_image.jpg)",
             },
+            fontFamily: {
+                Montserrat: ["Montserrat", "serif"],
+            },
         },
         colors: {
             logo: {
@@ -19,6 +22,11 @@ export default {
             copyright: "#9CA3AF",
             borderLight: "#FFFDFD",
             borderStrokeLight: "#D1D5DB",
+            error: {
+                900: "#FF0000",
+                600: "#FF3B3B",
+            },
+            delete: "#C60101",
         },
     },
     darkMode: "class",

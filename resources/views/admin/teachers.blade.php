40b0a2bf6f331754f44cb9d3d4c872fa738c8547
csc32 <sanzonettycar@gmail.com>
2023-12-28 23:52:15 -0400

    feat(web): add components to teachers views

--- resources/views/admin/teachers.blade.php
+++ resources/views/admin/teachers.blade.php
@@ -1,14 +1,29 @@
 <x-layout>
-    <main class="flex flex-row">
+    <main class="flex row">
         <x-aside>
             <x-logo />
-
             <x-navbar />
             <x-nav-bottom />
         </x-aside>
-        <section class="w-auto flex flex-col px-9 py-9">
+        @livewire('teacher-modal')
+        <section class="w-screen flex flex-col px-9 py-9">
             <x-title-component />
+            <section class="flex justify-center items-center">
+                <header class="w-full flex row items-center">
+                    <x-search />
+                    <livewire:btn-add :attrs="[
+                        'class' =>
+                            'flex row justify-evenly items-center w-44 h-10 px-2 py-2.5  bg-green-500 rounded-md gap-3.5  text-title font-semibold text-xl',
+                        'type' => 'button',
+                        'wire:click' => 'show',
+                    ]" />
+                </header>
+            </section>
+            <section class="my-6">
+                <livewire:teachers-table />
+            </section>
         </section>
+
     </main>
 
 </x-layout>
---
+++ resources/views/livewire/btn-add.blade.php
@@ -0,0 +1,7 @@
+<button {!! $customAttributes !!}><span>
+        Agregar
+    </span>
+    <span>
+        <x-heroicon-s-user-plus style="height: 21px" />
+    </span>
+</button>

f016efb9bcb617a2f7723c79c428d84503a46442
csc32 <sanzonettycar@gmail.com>
2023-12-28 23:49:32 -0400

    feat(traits): add traits for merge attributes
    add: new attribute :attrs to permit insert custom attributes to the
    components

---
+++ app/Livewire/BtnAdd.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire;
+
+use App\traits\ForwardsAttributes;
+use Livewire\Component;
+
+class BtnAdd extends Component
+{
+    use ForwardsAttributes;
+    public function mount(...$attrs)
+    {
+        $this->mapAttributes(...$attrs);
+    }
+
+    public function show()
+    {
+        $this->dispatch("show", "Aaaaaaah");
+    }
+
+    public function render()
+    {
+        return view('livewire.btn-add');
+    }
+}

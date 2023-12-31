f81d84cc2696a9244a35f15b59828468403e3136
csc32 <sanzonettycar@gmail.com>
2023-12-31 19:21:30 -0400

    add pagination to the table

--- app/Livewire/TeachersTable.php
+++ app/Livewire/TeachersTable.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TeachersTable extends Component
 {
+
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.teachers-table');
+        return view('livewire.teachers-table', ["teachers" => User::with("pfg")->where("rol_fk", 2)->Paginate(10)]);
     }
 }

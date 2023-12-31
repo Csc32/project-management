e1ba44a784b9f08be9261912740d5770d8a487a7
csc32 <sanzonettycar@gmail.com>
2023-12-31 19:38:15 -0400

    add styles to the table

--- resources/views/livewire/teachers-table.blade.php
+++ resources/views/livewire/teachers-table.blade.php
@@ -1,110 +1,45 @@
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
-        <thead class="text-xs text-white uppercase bg-blue-600 dark:text-white">
+    <table class="w-full min-h-[30rem] text-sm text-left rtl:text-right text-title-900 dark:text-title-900 ">
+        <thead class="text-xs text-white uppercase bg-desactivate bg dark:text-white">
             <tr>
                 <th scope="col" class="px-6 py-3">
-                    Product name
+                    NOMBRES </th>
+                <th scope="col" class="px-6 py-3">
+                    PFG
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Color
+                    CORREO
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Category
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Price
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Action
+                    Accíon
                 </th>
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-blue-500 border-b border-blue-400">
-                <th scope="row" class="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
-                <td class="px-6 py-4">
-                    <a href="#" class="font-medium text-white hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr class="bg-blue-600 border-b border-blue-400">
-                <th scope="row" class="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
-                    Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4">
-                    Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                    $1999
-                </td>
-                <td class="px-6 py-4">
-                    <a href="#" class="font-medium text-white hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr class="bg-blue-500 border-b border-blue-400">
-                <th scope="row" class="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
-                    Magic Mouse 2
-                </th>
-                <td class="px-6 py-4">
-                    Black
-                </td>
-                <td class="px-6 py-4">
-                    Accessories
-                </td>
-                <td class="px-6 py-4">
-                    $99
-                </td>
-                <td class="px-6 py-4">
-                    <a href="#" class="font-medium text-white hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr class="bg-blue-600 border-b border-blue-400">
-                <th scope="row" class="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
-                    Google Pixel Phone
-                </th>
-                <td class="px-6 py-4">
-                    Gray
-                </td>
-                <td class="px-6 py-4">
-                    Phone
-                </td>
-                <td class="px-6 py-4">
-                    $799
-                </td>
-                <td class="px-6 py-4">
-                    <a href="#" class="font-medium text-white hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr class="bg-blue-500 border-blue-40">
-                <th scope="row" class="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
-                    Apple Watch 5
-                </th>
-                <td class="px-6 py-4">
-                    Red
-                </td>
-                <td class="px-6 py-4">
-                    Wearables
-                </td>
-                <td class="px-6 py-4">
-                    $999
-                </td>
-                <td class="px-6 py-4">
-                    <a href="#" class="font-medium text-white hover:underline">Edit</a>
-                </td>
+            @if (empty($teachers))
+                <tr class="bg-bgLight border-b border-b-logo-500">
+                    No hay profesores registrados
+                </tr>
+            @else
+                @foreach ($teachers as $teacher)
+                    <tr class="bg-bgLight border-b border-b-logo-500">
+                        <td class="px-6 py-4">
+                            {{ $teacher->names }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $teacher->pfg->name }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $teacher->email }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $teacher->names }}
+                        </td>
+                    </tr>
+                @endforeach
+            @endif
             </tr>
         </tbody>
     </table>
+    {{ $teachers->links() }}
 </div>

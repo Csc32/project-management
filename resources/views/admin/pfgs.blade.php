6bfd28d823c7a562856d5ce3db65cc2287beeab2
csc32 <sanzonettycar@gmail.com>
2024-01-14 10:24:18 -0400

    update layout and prepare for add more elements

--- resources/views/admin/pfgs.blade.php
+++ resources/views/admin/pfgs.blade.php
@@ -8,12 +8,12 @@
         <section class="w-full flex flex-col px-9 py-9">
             <livewire:alert />
             <x-title-component />
-            <section class="flex flex-row justify-center items-start py-5">
+            <section class="flex flex-row justify-center items-start py-5 h-full">
                 {{--                     <x-search /> --}}
-                <section class="w-1/2 flex flex-col gap-1">
+                <section class="w-1/2 flex flex-col gap-5 py-5">
                     <article class="w-100">
-                        <div class="bg-borderLight w-full h-auto flex flex-col">
-                            <div class="w-full h-60">
+                        <div class="bg-borderLight w-full h-auto flex flex-col rounded-xl">
+                            <div class="w-full flex-1 py-4 px-2">
                                 <div id="studentChart">
                                     {!! $teacherChart->container() !!}
                                 </div>
@@ -21,8 +21,8 @@
                         </div>
                     </article>
                     <article class="w-100">
-                        <div class="bg-borderLight w-full h-auto flex flex-col">
-                            <div class="w-full h-60">
+                        <div class="bg-borderLight w-full h-auto flex flex-col rounded-xl">
+                            <div class="w-full flex-1 py-4 px-2">
                                 <div id="teacherChart">
                                     {!! $studentChart->container() !!}
                                 </div>
@@ -30,14 +30,16 @@
                         </div>
                     </article>
                 </section>
-                <section class="w-full flex row items-center flex-1">
-                    <livewire:search />
-                    <livewire:pfg-btn-add :attrs="[
-                        'class' =>
-                            'flex row justify-evenly items-center w-44 h-10 px-2 py-2.5  bg-green-500 rounded-md gap-3.5  text-title font-semibold text-xl',
-                        'type' => 'button',
-                        'wire:click' => 'show',
-                    ]" />
+                <section class="flex flex-col">
+                    <section class="w-full flex row items-center flex-1">
+                        <livewire:search />
+                        <livewire:pfg-btn-add :attrs="[
+                            'class' =>
+                                'flex row justify-evenly items-center w-44 h-10 px-2 py-2.5  bg-green-500 rounded-md gap-3.5  text-title font-semibold text-xl',
+                            'type' => 'button',
+                            'wire:click' => 'show',
+                        ]" />
+                    </section>
                 </section>
             </section>
         </section>

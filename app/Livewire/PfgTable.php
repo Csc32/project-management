6a84023fa0e1f0866770b374d83c2bab1e754426
csc32 <sanzonettycar@gmail.com>
2024-01-14 16:55:35 -0400

    feat(web): add search functionality

--- app/Livewire/PfgTable.php
+++ app/Livewire/PfgTable.php
@@ -5,15 +5,27 @@ namespace App\Livewire;
 use App\Models\Pfgs;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Livewire\Attributes\On;
+use Illuminate\Database\Eloquent\Builder;
 
 class PfgTable extends Component
 {
     use WithPagination;
     public $btnTitle = "Agregar";
+    public $searchValue = "";
 
+    #[On('searching')]
+    public function handleSearching($value)
+    {
+        $this->searchValue = $value;
+    }
     public function render()
     {
-        $pfgs = Pfgs::paginate(10);
+        $pfgs = Pfgs::query()
+            ->when($this->searchValue, function (Builder $query) {
+                $query->where("name", "like", $this->searchValue . "%");
+            })
+            ->paginate(10);
         return view('livewire.pfg-table', ['pfgs' => $pfgs]);
     }
 }

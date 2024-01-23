3a61a18f39422f6b7f69b3aeccfdf25a55471a87
csc32 <sanzonettycar@gmail.com>
2024-01-23 05:52:08 -0400

    feat(alert): update the class to display errors

--- app/Livewire/Alert.php
+++ app/Livewire/Alert.php
@@ -9,6 +9,15 @@ class Alert extends Component
 {
     public $message = '';
     public $show = 'hidden';
+    public $isError = false;
+    public $successColors = [
+        'alert-container' => "text-green-text bg-green-200 dark:bg-green-700 dark:text-green-100",
+        "btn" => "bg-green-200 text-green-text focus:ring-2 focus:ring-green-500 hover:bg-green-500 dark:bg-title-200 dark:text-green-700 dark:hover:bg-gray-700"
+    ];
+    public $errorColors = [
+        'alert-container' => "text-error-600 bg-error-200 dark:bg-error-900 dark:text-error-200",
+        "btn" => "bg-error-200 text-error-600 focus:ring-error-600 hover:bg-error-600 "
+    ];
 
     #[On('createdTeacher')]
     public function handleCreated($message)
@@ -40,12 +49,21 @@ class Alert extends Component
     #[On('delete')]
     public function handleDeleted($message)
     {
+        dd($message);
         $this->message = $message;
+        $this->show = "";
+    }
+    #[On('error')]
+    public function error($error)
+    {
+        $this->message = $error['message'];
+        $this->isError = true;
         $this->show = "";
     }
     public function close()
     {
         $this->show = "hidden";
+        $this->isError = false;
     }
     public function render()
     {

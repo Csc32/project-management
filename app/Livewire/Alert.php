4edd2886ceeee20ffcc16da1a7407043386aaadb
csc32 <sanzonettycar@gmail.com>
2024-01-23 20:22:03 -0400

    remove attribute because doesn't works

--- app/Livewire/Alert.php
+++ app/Livewire/Alert.php
@@ -10,15 +10,6 @@ class Alert extends Component
     public $message = '';
     public $show = 'hidden';
     public $isError = false;
-    public $successColors = [
-        'alert-container' => "text-green-text bg-green-200 dark:bg-green-700 dark:text-green-100",
-        "btn" => "bg-green-200 text-green-text focus:ring-2 focus:ring-green-500 hover:bg-green-500 dark:bg-title-200 dark:text-green-700 dark:hover:bg-gray-700"
-    ];
-    public $errorColors = [
-        'alert-container' => "text-error-600 bg-error-200 dark:bg-error-900 dark:text-error-200",
-        "btn" => "bg-error-200 text-error-600 focus:ring-error-600 hover:bg-error-600 "
-    ];
-
     #[On('createdTeacher')]
     public function handleCreated($message)
     {

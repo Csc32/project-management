c48d82ddf390e441232f6d795e0a94c7ac014aaf
csc32 <sanzonettycar@gmail.com>
2024-01-28 17:17:03 -0400

    add relationship from roles to users

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Roles extends Model
 {
@@ -21,5 +23,9 @@ class Roles extends Model
     public function permissions(): BelongsToMany
     {
         return $this->belongsToMany(Permissions::class);
+    }
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class, "rol_fk");
     }
 }

6e6e1c00e1ac039718eeaa97a97696ac92110726
csc32 <sanzonettycar@gmail.com>
2024-02-12 10:00:38 -0400

    feat(rolTable): add new method to filter by name

--- app/Livewire/Roles/RolTable.php
+++ app/Livewire/Roles/RolTable.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Roles;
 
 use App\Models\Roles;
+use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,9 +11,22 @@ class RolTable extends Component
 {
     use WithPagination;
 
+    public string $searchValue = '';
+
+    #[On('searching')]
+    public function handleSearching($value)
+    {
+        $this->searchValue = $value;
+    }
+
     public function render()
     {
-        $roles = Roles::paginate(5);
+
+        $roles = Roles::with('users')
+            ->when($this->searchValue !== '', function ($query) {
+                $query->where('roles.name', 'like', $this->searchValue.'%');
+            })
+            ->paginate(5);
 
         return view('livewire.roles.rol-table', ['roles' => $roles]);
     }

cdb709cc3af264515aa3cacb61d3689e3b3f5336
csc32 <sanzonettycar@gmail.com>
2024-02-12 10:04:30 -0400

    add filter and btn to add new role

--- resources/views/admin/roles.blade.php
+++ resources/views/admin/roles.blade.php
@@ -8,7 +8,17 @@
         </x-aside>
         <section class="w-full flex flex-col px-9 py-9">
             <x-title-component />
-            <section class="w-full flex flex-col justify-evenly items-center gap-5">
+            <header class="w-full flex row items-center flex-shrink">
+                {{--                     <x-search /> --}}
+                <livewire:search />
+                <livewire:btn-add :attrs="[
+                    'class' =>
+                        'flex row justify-evenly items-center w-44 h-10 px-2 py-2.5  bg-green-500 rounded-md gap-3.5  text-title font-semibold text-xl',
+                    'type' => 'button',
+                    'wire:click' => 'show',
+                ]" />
+            </header>
+            <section class="w-full flex flex-col justify-evenly items-center gap-05">
                 <livewire:roles.rol-table />
                 <livewire:roles.charts.role-chart />
             </section>
